293edd93b8b4cdf09d23ebcedd34b388da9b5e30
Mikael Koli <koli.mikael@gmail.com>
2019-04-24 21:04:18 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="EcoSys",
+    version="0.1",
+    packages=find_packages(),
+    long_description=open('README.md').read(),
+    author="Mikael Koli",
+    author_email="koli.mikael@gmail.com",
+)
